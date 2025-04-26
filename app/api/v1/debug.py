f440fd683883e3260fabdbbdccefa60bee1beab9
frohlich71 <victorfrohlich520@gmail.com>
2025-04-26 11:47:26 -0300

    feat: debug the db file in the new directory

--- app/api/v1/debug.py
+++ app/api/v1/debug.py
@@ -5,7 +5,7 @@ router = APIRouter()
 
 @router.get("/tables")
 def get_tables():
-    conn = sqlite3.connect("local.db")
+    conn = sqlite3.connect("/app/db/local.db")
     cursor = conn.cursor()
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
     return {"tables": cursor.fetchall()}
